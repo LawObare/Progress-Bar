d451261ef032ad96c3d1e37d33a36563a76f555c
Lawrence Obare <obarelawrence.acc@gmail.com>
2026-07-09 21:49:32 +0300

    Chore: Dashboard change

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -1,6 +1,7 @@
 import {Outlet} from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
+import 
 
 function DashboardLayout() {
     return (
